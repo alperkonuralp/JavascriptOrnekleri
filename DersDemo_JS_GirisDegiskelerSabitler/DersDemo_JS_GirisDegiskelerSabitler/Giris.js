39a22a67275cf55967517ee7752163cdac59492b
Alper Konuralp - YU7101 <alper@konuralp.gen.tr>
2010-07-20 16:49:50 +0300

    Javascript'e giriş dersimiz.

---
+++ DersDemo_JS_GirisDegiskelerSabitler/DersDemo_JS_GirisDegiskelerSabitler/Giris.js
@@ -0,0 +1,106 @@
+﻿document.body.style.backgroundColor = '#1d2f3e';
+//document.writeln("Merhaba Dünya");
+/*
+yaz(); // fonksiyonumuzu çağırıyoruz.
+
+function yaz() {
+document.writeln("Merhaba Dünya");
+}
+*/
+
+var a = 5;
+var A = 4;
+
+
+var a = "Bu ne?";
+
+a = 110.0;
+a = " ";
+
+var b = true;
+var b = false;
+
+var True = 1;
+
+var Ikinci_Degisken = true;
+
+var c = "Bu bir denemedir.";
+var d = 'Bu bir denemedir.';
+
+var e = "2";
+
+var f = "\"Bu dünyaya geldim.\" dedi.";
+
+var g = "Bu\nBir\n\tDenemdir.";
+/*var h = "Bu 
+Bir
+Denemedir.";
+*/
+var j = '\'dur orada\' dedi.';
+var j = '"dur orada" dedi.';
+var j = "'dur orada' dedi.";
+
+var i = "1420\\5 sk. no:12";
+//alert(g);
+
+var i = null;
+
+var yas = 32;
+yas = "Yaşım  " + yas;
+
+var yas = 32;
+var yas2 = "Yaşım " + yas;
+yas = yas2;
+
+
+//var toplam = 0;
+//for (var i = 0; i < 10; /*i = i + 1*/ /*i += 1*/i++) {
+//    var sayi = prompt(i + ". Sayıyı Giriniz :", "0");
+//    toplam += parseInt(sayi);
+//}
+
+//alert("Sayıların Toplamı : " + toplam);
+
+var ttt = 10;
+var uuu = 5;
+
+var vvv = ttt + uuu;
+ttt += uuu;
+
+//alert(ttt);
+
+//alert(1 == 1);
+//alert(1 != 1);
+
+//alert("a" == "A");
+//alert("aaa" == "aaaa");
+//alert("aaa" >= "aaaa");
+
+//alert("alper" > "ali");
+//alert("alper" > "Ali");
+
+//alert("c .aaaaaaaaaa" < "bb");
+
+
+//alert(ttt > uuu && uuu == 5);
+// T && T == T
+// T && F == F
+// F && T == F 
+// F && F == F
+
+//alert(ttt == 10 || ttt == 15);
+// T || T == T
+// T || F == T
+// F || T == T
+// F || F == F
+
+//alert(25 * 3 - 14 / 2);
+
+// ()
+// * /
+// + -
+// &&
+// ||
+// =
+
+alert(ttt == 10 && uuu == 5 || uuu == 10);
